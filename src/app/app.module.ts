187814ddbe70da771075d68993c810b5c037c349
Ed O'Connor <ed@oconnor.app>
2023-02-12 15:27:15 -0500

    css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StateListComponent } from './state-list/state-list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TitleCasePipe } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -14,12 +13,13 @@ import {MatButtonModule} from '@angular/material/button';
 import { ScoreComponent } from './score/score.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
  
  
  
 
 @NgModule({
-  declarations: [AppComponent, StateListComponent, ScoreComponent, QuizComponent, HomeComponent ],
+  declarations: [AppComponent, ScoreComponent, QuizComponent, HomeComponent, NavbarComponent ],
   imports: [
     BrowserModule,
     AppRoutingModule,
