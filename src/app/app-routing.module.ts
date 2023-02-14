a2dc7931eb36b693d7ae2b603bf9a0e2b0708218
Ed O'Connor <ed@oconnor.app>
2023-02-14 08:59:43 -0500

    nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { QuizComponent } from './quiz/quiz.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'quiz', pathMatch: 'full' },
-  { path: 'quiz', component: QuizComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'quiz', component: QuizComponent },
   { path: 'score', component: ScoreComponent },
 ];
 
