6075c9cb5e2c5668783099d1eab71214881defba
Ed O'Connor <ed@oconnor.app>
2023-02-23 16:38:28 -0500

    local data edit

--- src/app/state.service.ts
+++ src/app/state.service.ts
@@ -1,16 +1,45 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { State } from './state';
+import { NgbTypeaheadConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StateService {
-  url = 'https://state-backend.herokuapp.com';
+  url = '/assets/states.json';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getStates() {
-    return this.http.get<State[]>(`${this.url}/states`);
+  getStates(): Observable<State[]> {
+    return this.http.get<State[]>(this.url).pipe(
+      catchError(() => {
+        return of([]);
+      })
+    );
   }
 }
+
+// <---------------------------------------------->
+// The code above uses a local copy of data rather
+// than making an API call. See below for API call:
+// <---------------------------------------------->
+
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { State } from './state';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class StateService {
+//   url = 'https://state-backend.herokuapp.com';
+
+//   constructor(private http: HttpClient) { }
+
+//   getStates() {
+//     return this.http.get<State[]>(`${this.url}/states`);
+//   }
+// }
